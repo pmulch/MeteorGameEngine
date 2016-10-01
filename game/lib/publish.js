403d741d3e10f149d8523b3d6889dff77ac35237
Puneet Mulchandani <pm.mulchandani@gmail.com>
2016-10-01 18:53:14 -0400

    added publications

---
+++ game/lib/publish.js
@@ -0,0 +1,39 @@
+/**
+ * publish.js
+ * scope: server
+ *
+ * Manages all publications and modifications of collections
+ */
+
+// TODO: update the security policy on this collection
+
+// publish all games to all users (for now)
+Meteor.publish('games', function(accessCodeOrId) { 
+	
+	// fetch by id or by access code	
+	if ( _(accessCodeOrId).isString() && accessCodeOrId !== '' ) {
+		return Games.find({ 
+			$or: [ 
+				{ _id: accessCodeOrId },
+				{ accessCode: accessCodeOrId }
+			] 
+		});
+	}
+
+	// fetch all
+	return Games.find({});		// TODO: maybe restrict this if not needed?
+});
+
+
+// for now, allow insert / update operations, but not delete
+Games.allow({
+	insert: function(userId, doc) {
+		return true;
+	}, 
+	update: function(userId, doc) {
+		return true;
+	},
+	remove: function(userId, doc) {
+		return false;
+	}
+});
