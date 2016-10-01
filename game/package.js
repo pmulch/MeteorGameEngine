fb0e6627ec48dec0a3e6d8cef21122f463e36ba7
Puneet Mulchandani <pm.mulchandani@gmail.com>
2016-10-01 19:35:00 -0400

    updated package.js declaration; temporarily commented out tests

--- game/package.js
+++ game/package.js
@@ -11,14 +11,37 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
+  // min version
   api.versionsFrom('1.4.1.1');
+
+  // dependencies
   api.use(['ecmascript', 'underscore']);
-  api.mainModule('game.js');
+
+  // common
+  api.addFiles([
+    'lib/utils.js', 
+    'lib/data.js'
+  ], ['client', 'server']);
+
+  // server
+  api.addFiles([
+    'lib/publish.js',
+    'lib/game-server.js'
+  ], ['server']);
+
+  // client
+  api.addFiles([
+    'lib/game-controller.js',
+    'lib/game-session.js'
+  ], ['client']);
+
+  // exports
+  api.export(['GameSession', 'GameController', 'Games', 'Game'], ['client', 'server']);
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('pmulch:game');
-  api.mainModule('game-tests.js');
+  // api.use('ecmascript');
+  // api.use('tinytest');
+  // api.use('pmulch:game');
+  // api.mainModule('game-tests.js');
 });
