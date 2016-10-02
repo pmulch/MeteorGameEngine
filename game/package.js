0dd4e8ee2a4a1dfd29a999f3e9e12d25b0a38f88
Puneet Mulchandani <pm.mulchandani@gmail.com>
2016-10-02 11:38:28 -0400

    clean up

--- game/package.js
+++ game/package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'pmulch:game',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'A framework for building bring-your-own-device (BYOD) games using Meteor',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/pmulch/MeteorGameEngine.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
